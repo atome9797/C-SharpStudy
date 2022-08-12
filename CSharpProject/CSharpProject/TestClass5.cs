9b8a5594ab6ce4d30bac4358fbe6cc58b784f37e
ProgrammerPenguin <65073631+atome9797@users.noreply.github.com>
2022-08-12 17:42:27 +0900

    Attribute 예제풀이

---
+++ CSharpProject/CSharpProject/TestClass5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+interface IFlyable { void Fly(); }
+
+
+struct Foo : IFlyable
+{
+    public void Fly() { }
+}
+
+int i = 10; // Value Type
+object obj = i; // Reference Type으로 받음 => Boxing
+int a = (int)obj; //Unboxing, 10
+
+Foo f = new Foo();//Value Type 
+IFlyable f2 = f;//Reference Type => Boxing
+
+
---
+++ CSharpProject/CSharpProject/TestClass6.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace testVersion
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    class History : Attribute
+    {
+        private string programmer;
+        private double version;
+        private string changes;
+
+        //생성자
+        public History(string programmer, double version, string changes)
+        {
+            this.programmer = programmer;
+            version = 1.0;
+            changes = "First release";
+        }
+
+        public string GetProgrammer()
+        {
+            return programmer;
+        }
+
+        public double GetVersion()
+        {
+            return version;
+        }
+
+        public string Getchanges()
+        {
+            return changes;
+        }
+    }
+
+    [History("Sean", 0.1, "2017-11-01 Created stub")]
+    [History("Bob", 0.2, "2020-12-03 Added Func() Method")]
+    class MyClass
+    {
+        public void Func()
+        {
+            Console.WriteLine("Func()");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(MyClass);
+
+            Attribute[] attributes = Attribute.GetCustomAttributes(type);
+
+            Console.WriteLine("MyClass change history");
+
+            foreach (Attribute a in attributes)
+            {
+                History h = a as History;
+                if (h != null)
+                {
+                    Console.WriteLine($"Ver : {h.GetVersion()}, Programmer : {h.GetProgrammer()} , Changes : {h.Getchanges()}");
+                }
+            }
+        }
+    }
+}
